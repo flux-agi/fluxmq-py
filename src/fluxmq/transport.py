3cea04bce456a2d843c324cd5243fbf15cf396d9
Eugene Zimnitskiy <eugene@Eugene-Mac.local>
2025-01-30 12:56:54 +0300

    Add sync trasport

--- src/fluxmq/transport.py
+++ src/fluxmq/transport.py
@@ -34,3 +34,37 @@ class Transport(ABC):
         pass
 
     
+    from abc import ABC, abstractmethod
+from queue import Queue
+
+from fluxmq.message import Message
+
+
+class SyncTransport(ABC):
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    @abstractmethod
+    def publish(self, topic: str, payload: bytes) -> None:
+        pass
+
+    @abstractmethod
+    def subscribe(self, topic: str) -> Queue[Message]:
+        pass
+
+    @abstractmethod
+    def unsubscribe(self, topic: str) -> None:
+        pass
+
+    @abstractmethod
+    def request(self, topic: str, payload: bytes) -> None:
+        pass
+
+    @abstractmethod
+    def respond(self, message: Message, response: bytes) -> None:
+        pass
