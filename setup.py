068b2e25a172dc9dfb49cfdb5a95daa0a839542f
Eugene Zimnitskiy <eugene@Eugene-Mac.local>
2025-02-19 14:39:51 +0300

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="fluxmq",
-    version="0.2.0",
-    packages=find_packages(),
+    version="0.3.0",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=[],
 )
