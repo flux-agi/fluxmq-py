8191ec2a3755794699202f4f4387030f4317e3b4
Eugene Zimnitskiy <eugene@Eugene-Mac.local>
2025-02-19 19:42:17 +0300

    Update

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+install_requires = [
+    "nats-py>=2.0.0",
+]
+
+import sys
+if sys.version_info < (3, 7):
+    install_requires.append("dataclasses")
+
 setup(
     name="fluxmq",
     version="0.3.0",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=[
-        "nats-py>=2.0.0"
-    ],
+    install_requires=install_requires,
 )
